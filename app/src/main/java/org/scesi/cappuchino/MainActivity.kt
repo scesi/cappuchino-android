d702fa47cbe7f4095c44ca0177f407c935325ff4
MauriNestor <mauric845apaza@gmail.com>
2024-11-01 15:12:45 -0400

    add navegacion y librerias

--- app/src/main/java/org/scesi/cappuchino/MainActivity.kt
+++ app/src/main/java/org/scesi/cappuchino/MainActivity.kt
@@ -4,44 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import org.scesi.cappuchino.ui.theme.CappuchinoTheme
+import org.scesi.cappuchino.ui.Cappuchino
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CappuchinoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Cappuchino()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CappuchinoTheme {
-        Greeting("Android")
     }
 }

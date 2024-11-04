1aced15b1ee5b0578082d4779173fee383264560
Saul <saulcordovavill@gmail.com>
2024-11-03 21:00:08 -0400

    configuracion inicial del settings screen

--- app/src/main/java/org/scesi/cappuchino/ui/screens/SettingsScreen.kt
+++ app/src/main/java/org/scesi/cappuchino/ui/screens/SettingsScreen.kt
@@ -1,179 +1,134 @@
 package org.scesi.cappuchino.ui.screens
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.ui.Modifier
+import androidx.compose.material3.DropdownMenuItem
+
+package org.scesi.cappuchino
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.Icon
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.scesi.cappuchino.R
-import org.scesi.cappuchino.ui.theme.CappuchinoTheme
+import java.lang.reflect.Modifier
 
 @Composable
-fun SettingsScreen(){
-    SettingsScreenContent()
-}
-
-@Composable
-private fun SettingsScreenContent(){
+fun settingsScreen(){
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        Column() {
-            Text(
-                text = stringResource(R.string.configuracion),
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp
-            )
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Text(text = "Configuraciones", fontSize = 20.sp)
 
-            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_padding_size_medium)))
+        Spacer(modifier = Modifier.height(16.dp))
 
-            Text(text = stringResource(R.string.mantener_horario_al_cerrar_el_navegador), modifier = Modifier.padding(8.dp))
+        Text(text = "mantener El horario al cerrar el navegador")
 
-            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_padding_size_medium)))
+        Spacer(modifier = Modifier.height(16.dp))
 
-            Text(text = stringResource(R.string.modo_oscuro), modifier = Modifier.padding(8.dp))
+        Text(text ="Modo Oscuro")
 
-            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_padding_size_medium)))
+        Spacer(modifier = Modifier.height(32.dp))
 
-            Text(
-                text = stringResource(R.string.configuracion_visual),
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp
-            )
+        Text(
+            text = "Configuración Visual",
+            fontWeight = FontWeight.Bold
+        )
 
-            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_padding_size_medium)))
+        Spacer(modifier = Modifier.height(16.dp))
 
-            ConfigurationRow(
-                title = stringResource(R.string.tama_o_de_la_fuente),
-                text = "Mediano"
-            )
+        Text(text = "Tamaño de la fuente")
+        FZDropDown()
 
-            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_padding_size_medium)))
+        Text(text = "Estilo de la fuente")
+        SFDropDown()
 
-            ConfigurationRow(title = stringResource(R.string.estilo_de_fuente),
-                text ="Calibri"
-            )
-
-            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_padding_size_medium)))
-            Text(text = stringResource(R.string.temas))
-        }
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.End
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            CappuchinoButton(
-                title = stringResource(R.string.cancelar),
-                contentColor = Color.Black,
-                contarinerColor = Color.White,
-                borderColor = Color.Gray
-            )
-
-            Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.margin_padding_size_small)))
-
-            CappuchinoButton(
-                title = stringResource(R.string.guardar),
-                contentColor = Color.White,
-                contarinerColor = Color.Black,
-                borderColor = Color.Black,
-            )
+            Button(onClick = {  }, colors = ButtonDefaults.outlinedButtonColors()) {
+                Text(text ="Cancelar")
+            }
+            Button(onClick = {  }) {
+                Text(text ="Guardar")
+            }
         }
     }
 }
 
-@Composable
-fun ConfigurationRow(
-    title:String,
-    text: String
-){
-    Row{
-        Text(
-            text = title,
-            modifier = Modifier
-                .padding(dimensionResource(id = R.dimen.margin_padding_size_small))
-                .weight(1f)
-        )
 
-        CapuchinDropDown(text = text, modifier = Modifier.weight(1f))
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FZDropDown(){
+    val listsize = listOf("Pequeño", "Mediano", "Grande")
+    var expanded by remember { mutableStateOf(false)}
+    var selectedText by remember { mutableStateOf(listsize[0])}
+
+
+    ExposedDropdownMenuBox(
+        expanded = false, onExpandedChange = {expanded = !expanded}
+    )
+    {
+        TextField(
+            value = selectedText
+            ,onValueChange = {},
+            readOnly = true,
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
+        )
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            listsize.forEachIndexed { index, text ->
+                DropdownMenuItem(text = { Text(text = text) }, onClick = {
+                    selectedText = listsize[index]
+                    expanded = false
+                },
+                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,)
+            }
+        }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CapuchinDropDown(text: String, modifier: Modifier){
-    Row(
-        modifier = modifier
-            .border(
-                width = dimensionResource(id = R.dimen.border_line),
-                color = Color.Gray,
-                shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius_small))
-            )
-            .wrapContentHeight()
-    ){
-        Text(
-            text = text, modifier = Modifier
-                .weight(3f)
-                .align(Alignment.CenterVertically), textAlign = TextAlign.Center
-        )
-        Icon(
-            imageVector = Icons.Default.ArrowDropDown,
-            contentDescription = "Drop Down Icon",
-            modifier = Modifier
-                .border(
-                    width = dimensionResource(id = R.dimen.border_line),
-                    color = Color.Gray,
-                    shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius_small))
-                )
-        )
-    }
-}
+fun SFDropDown(){
+    val listsize = listOf("Calibri", "Arial", "Otro")
+    var expanded by remember { mutableStateOf(false)}
+    var selectedText by remember { mutableStateOf(listsize[0])}
 
-@Composable
-fun CappuchinoButton(
-    title: String,
-    contentColor: Color,
-    contarinerColor: Color,
-    borderColor: Color
-){
-    Button(onClick = {  },
-        colors = ButtonDefaults.outlinedButtonColors(
-            contentColor = contentColor,
-            containerColor = contarinerColor
-        ),border = BorderStroke(dimensionResource(id = R.dimen.border_line), borderColor),
-        shape = RoundedCornerShape(dimensionResource(id = R.dimen.margin_padding_size_small))
-    ) {
-        Text(title)
-    }
-}
 
-@Preview(showBackground = true)
-@Composable
-private fun SettingsScreenPreview(){
-    CappuchinoTheme {
-        SettingsScreen()
+    ExposedDropdownMenuBox(
+        expanded = false, onExpandedChange = {expanded = !expanded}
+    )
+    {
+        TextField(
+            value = selectedText
+            ,onValueChange = {},
+            readOnly = true,
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
+        )
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            listsize.forEachIndexed { index, text ->
+                DropdownMenuItem(text = { Text(text = text) }, onClick = {
+                    selectedText = listsize[index]
+                    expanded = false
+                },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,)
+            }
+        }
     }
 }

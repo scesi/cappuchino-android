3d07d5a5327203c99883221b2210283e797caf84
Saul <saulcordovavill@gmail.com>
2024-11-07 19:03:32 -0400

    Add indentation and spacing to text; redesign buttons for improved UI

--- app/src/main/java/org/scesi/cappuchino/ui/screens/SettingsScreen.kt
+++ app/src/main/java/org/scesi/cappuchino/ui/screens/SettingsScreen.kt
@@ -1,19 +1,24 @@
 package org.scesi.cappuchino.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
-
-package org.scesi.cappuchino
-
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import org.scesi.cappuchino.R
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.DropdownMenu
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -21,50 +26,88 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.lang.reflect.Modifier
 
+@Preview( showBackground = true)
 @Composable
-fun settingsScreen(){
+fun SettingsScreen(){
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ){
-        Text(text = "Configuraciones", fontSize = 20.sp)
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
 
-        Spacer(modifier = Modifier.height(16.dp))
+        ) {
+            Text(
+                text = "CONFIGURACION",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
 
-        Text(text = "mantener El horario al cerrar el navegador")
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Mantener horario al cerrar el navegador", modifier = Modifier.padding(8.dp))
 
-        Text(text ="Modo Oscuro")
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(32.dp))
+            Text(text = "Modo Oscuro", modifier = Modifier.padding(8.dp))
 
-        Text(
-            text = "Configuración Visual",
-            fontWeight = FontWeight.Bold
-        )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "CONFIGURACION VISUAL",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
 
-        Text(text = "Tamaño de la fuente")
-        FZDropDown()
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Estilo de la fuente")
-        SFDropDown()
+            Row {
+                Text(text = "Tamaño de la fuente",
+                    modifier = Modifier.padding(8.dp))
 
+                DropDownTamanioFuente()
+
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Estilo de fuente",
+                    modifier = Modifier.padding(8.dp))
+
+                DropDownStyles()
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Temas")
+        }
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.End
         ) {
-            Button(onClick = {  }, colors = ButtonDefaults.outlinedButtonColors()) {
+            Button(onClick = {  },
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.Black,
+                    containerColor = Color.White
+                ),border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(8.dp)
+            ) {
                 Text(text ="Cancelar")
             }
-            Button(onClick = {  }) {
+            Button(onClick = {  },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black
+                ), shape = RoundedCornerShape(8.dp),
+                modifier = Modifier.padding(8.dp)
+            ){
                 Text(text ="Guardar")
             }
         }
@@ -75,7 +118,7 @@ fun settingsScreen(){
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FZDropDown(){
+fun DropDownTamanioFuente(){
     val listsize = listOf("Pequeño", "Mediano", "Grande")
     var expanded by remember { mutableStateOf(false)}
     var selectedText by remember { mutableStateOf(listsize[0])}
@@ -105,7 +148,7 @@ fun FZDropDown(){
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SFDropDown(){
+fun DropDownStyles(){
     val listsize = listOf("Calibri", "Arial", "Otro")
     var expanded by remember { mutableStateOf(false)}
     var selectedText by remember { mutableStateOf(listsize[0])}

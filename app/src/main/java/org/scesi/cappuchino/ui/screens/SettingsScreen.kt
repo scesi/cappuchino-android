a11fdce4f9b42e5d3bb9e59448205e217697fa01
Saul <saulcordovavill@gmail.com>
2024-11-21 00:04:14 -0400

    [CAP-005] add function CapuchinoDropDown

--- app/src/main/java/org/scesi/cappuchino/ui/screens/SettingsScreen.kt
+++ app/src/main/java/org/scesi/cappuchino/ui/screens/SettingsScreen.kt
@@ -1,11 +1,11 @@
 package org.scesi.cappuchino.ui.screens
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,21 +13,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -72,18 +71,13 @@ private fun SettingsScreenContent(){
 
             ConfigurationRow(
                 title = stringResource(R.string.tama_o_de_la_fuente),
-                listsize = listOf(
-                    stringResource(R.string.peque_o),
-                    stringResource(R.string.mediano),
-                    "Grande")
+                "Mediano"
             )
 
             Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_padding_size_medium)))
 
             ConfigurationRow(title = stringResource(R.string.estilo_de_fuente),
-                listsize = listOf(stringResource(R.string.calibri),
-                    "Puppets",
-                    "Arial")
+                "Calibri"
             )
 
             Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_padding_size_medium)))
@@ -97,14 +91,17 @@ private fun SettingsScreenContent(){
                 title = stringResource(R.string.cancelar),
                 contentColor = Color.Black,
                 contarinerColor = Color.White,
-                borderColor = Color.Gray)
+                borderColor = Color.Gray
+            )
+
+            Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.margin_padding_size_small)))
 
             CappuchinoButton(
                 title = stringResource(R.string.guardar),
                 contentColor = Color.White,
                 contarinerColor = Color.Black,
-                borderColor = Color.Black)
-
+                borderColor = Color.Black,
+            )
         }
     }
 }
@@ -112,41 +109,46 @@ private fun SettingsScreenContent(){
 @Composable
 fun ConfigurationRow(
     title:String,
-    listsize: List<String>
+    text: String
 ){
-    Row {
+    Row{
         Text(
             text = title,
-            modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_padding_size_small))
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.margin_padding_size_small))
+                .weight(1f)
         )
 
-        DropDown(listsize)
+        CapuchinDropDown(text = text, modifier = Modifier.weight(1f))
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DropDown(listsize: List<String>){
-    var expanded by remember { mutableStateOf(false)}
-    var selectedText by remember { mutableStateOf(listsize[0])}
-    ExposedDropdownMenuBox(
-        expanded = false, onExpandedChange = {expanded = !expanded}
-    )
-    {
-        TextField(
-            value = selectedText
-            ,onValueChange = {},
-            readOnly = true,
-            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
+fun CapuchinDropDown(text: String, modifier: Modifier){
+    Row(
+        modifier = modifier
+            .border(
+                width = dimensionResource(id = R.dimen.border_line),
+                color = Color.Gray,
+                shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius_small))
+            )
+            .wrapContentHeight()
+    ){
+        Text(
+            text = text, modifier = Modifier
+                .weight(3f)
+                .align(Alignment.CenterVertically), textAlign = TextAlign.Center
         )
-        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-            listsize.forEachIndexed { index, text ->
-                DropdownMenuItem(text = { Text(text = text) }, onClick = {
-                    selectedText = listsize[index]
-                    expanded = false },
-                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,)
-            }
-        }
+        Icon(
+            imageVector = Icons.Default.ArrowDropDown,
+            contentDescription = "Drop Down Icon",
+            modifier = Modifier
+                .border(
+                    width = dimensionResource(id = R.dimen.border_line),
+                    color = Color.Gray,
+                    shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner_radius_small))
+                )
+        )
     }
 }
 
@@ -161,7 +163,7 @@ fun CappuchinoButton(
         colors = ButtonDefaults.outlinedButtonColors(
             contentColor = contentColor,
             containerColor = contarinerColor
-        ),border = BorderStroke(1.dp, borderColor),
+        ),border = BorderStroke(dimensionResource(id = R.dimen.border_line), borderColor),
         shape = RoundedCornerShape(dimensionResource(id = R.dimen.margin_padding_size_small))
     ) {
         Text(title)

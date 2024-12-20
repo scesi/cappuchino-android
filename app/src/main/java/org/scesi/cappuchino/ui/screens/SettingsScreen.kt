235ce6e87caad3dd37665ff235eb1791311f9d94
Saul <saulcordovavill@gmail.com>
2024-12-19 21:27:22 -0400

    [CAP-014] Refactor Strings

--- app/src/main/java/org/scesi/cappuchino/ui/screens/SettingsScreen.kt
+++ app/src/main/java/org/scesi/cappuchino/ui/screens/SettingsScreen.kt
@@ -19,14 +19,15 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
@@ -46,8 +47,15 @@ fun SettingsScreen(){
 
 @Composable
 private fun SettingsScreenContent(){
-    val sources = listOf("Arial", stringResource(id = R.string.Calibri), "Poppins")
-    val sizes = listOf(stringResource(id = R.string.Pequeño), stringResource(id = R.string.Mediano), "Grande")
+    val sources = listOf(
+        stringResource(id = R.string.arial), 
+        stringResource(id = R.string.Calibri), 
+        stringResource(id = R.string.Poppins)
+    )
+    val sizes = listOf(
+        stringResource(id = R.string.Pequeño), 
+        stringResource(id = R.string.Mediano), 
+        stringResource(id = R.string.Grande))
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween
@@ -121,7 +129,7 @@ private fun SettingsScreenContent(){
             CappuchinoButton(
                 title = stringResource(R.string.cancelar),
                 contentColor = Color.Black,
-                contarinerColor = Color.White,
+                containerColor = Color.White,
                 borderColor = Color.Gray
             )
 
@@ -133,7 +141,7 @@ private fun SettingsScreenContent(){
             CappuchinoButton(
                 title = stringResource(R.string.guardar),
                 contentColor = Color.White,
-                contarinerColor = Color.Black,
+                containerColor = Color.Black,
                 borderColor = Color.Black,
             )
         }
@@ -161,8 +169,8 @@ fun ConfigurationRow(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CapuchinDropDown(text: String, modifier: Modifier, stringList: List<String>){
-    val bottomSheetState = remember { mutableStateOf(false) }
-    val textSelected = remember { mutableStateOf(text)}
+    var bottomSheetState by remember { mutableStateOf(false) }
+    var textSelected by remember { mutableStateOf(text)}
     Row(
         modifier = modifier
             .border(
@@ -176,11 +184,11 @@ fun CapuchinDropDown(text: String, modifier: Modifier, stringList: List<String>)
         Row (
             modifier = Modifier
                 .clickable {
-                    bottomSheetState.value = true
+                    bottomSheetState = true
                 }
         ){
             Text(
-                text = textSelected.value, modifier = Modifier
+                text = textSelected, modifier = Modifier
                     .weight(3f)
                     .align(Alignment.CenterVertically)
                     , textAlign = TextAlign.Center
@@ -198,9 +206,9 @@ fun CapuchinDropDown(text: String, modifier: Modifier, stringList: List<String>)
         }
 
 
-        if(bottomSheetState.value) {
+        if(bottomSheetState) {
             ModalBottomSheet(
-                onDismissRequest = { bottomSheetState.value = false }
+                onDismissRequest = { bottomSheetState = false }
             ) {
                 Column(
                     modifier = Modifier
@@ -213,8 +221,8 @@ fun CapuchinDropDown(text: String, modifier: Modifier, stringList: List<String>)
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .clickable {
-                                    textSelected.value = stringList
-                                    bottomSheetState.value = false
+                                    textSelected = stringList
+                                    bottomSheetState = false
                                 }
                                 .padding(dimensionResource(id = R.dimen.margin_padding_size_small)),
                             textAlign = TextAlign.Center
@@ -231,13 +239,13 @@ fun CapuchinDropDown(text: String, modifier: Modifier, stringList: List<String>)
 fun CappuchinoButton(
     title: String,
     contentColor: Color,
-    contarinerColor: Color,
+    containerColor: Color,
     borderColor: Color
 ){
     Button(onClick = {  },
         colors = ButtonDefaults.outlinedButtonColors(
             contentColor = contentColor,
-            containerColor = contarinerColor
+            containerColor = containerColor
         ),border = BorderStroke(dimensionResource(id = R.dimen.border_line), borderColor),
         shape = RoundedCornerShape(dimensionResource(id = R.dimen.margin_padding_size_small))
     ) {

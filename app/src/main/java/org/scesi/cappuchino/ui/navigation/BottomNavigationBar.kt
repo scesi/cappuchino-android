899052b2adfeed3a28262b4961e295724011d4cd
MauriNestor <mauric845apaza@gmail.com>
2025-03-31 16:48:24 -0400

    [CAP-37] add styles in bottomnavbar

--- app/src/main/java/org/scesi/cappuchino/ui/navigation/BottomNavigationBar.kt
+++ app/src/main/java/org/scesi/cappuchino/ui/navigation/BottomNavigationBar.kt
@@ -1,12 +1,27 @@
 package org.scesi.cappuchino.ui.navigation
 
-import android.util.Log
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.scesi.cappuchino.R
+
 
 @Composable
 fun BottomNavigationBar(
@@ -15,22 +30,44 @@ fun BottomNavigationBar(
     selectedItemIndex: Int
 
 ) {
-    NavigationBar{
+    NavigationBar(
+        containerColor = Color.White,
+        ) {
         bottomNavOptions.forEachIndexed { index, screen ->
             NavigationBarItem(
-                selected = selectedItemIndex == index ,
+                selected = index == selectedItemIndex,
                 onClick = {
                     onNavBarItemClicked(screen)
                 },
                 icon = {
-                    Log.d("icon", "Index: $index, Selected: $selectedItemIndex, IsSelected: ${selectedItemIndex == index}")
                     Icon(
-                        painter = painterResource(id = if (index == selectedItemIndex ) screen.iconSelected else screen.iconUnselected),
-                        contentDescription = screen.route
+                        painter = painterResource(id = if (index == selectedItemIndex) screen.iconSelected else screen.iconUnselected),
+                        contentDescription = screen.route,
+                        modifier = Modifier.size(dimensionResource(id = R.dimen.icon_size))
                     )
                 },
-                label = { Text(screen.route) }
+                label = { Text(screen.route) },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = LocalContentColor.current.copy(alpha = 0.2f),
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    unselectedTextColor = LocalContentColor.current.copy(alpha = 0.2f),
+                    indicatorColor = Color.White
+                ),
+
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomNavigationBarPreview() {
+    val selectedItemIndex = remember { mutableStateOf(0) }
+
+    BottomNavigationBar(
+        bottomNavOptions = bottomNavBarItems,
+        onNavBarItemClicked = { },
+        selectedItemIndex = selectedItemIndex.value
+    )
 }

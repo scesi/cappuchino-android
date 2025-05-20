0e881c98ba537dc062ed0bc3a21d2877ca228fec
MauriNestor <mauric845apaza@gmail.com>
2025-05-19 20:59:08 -0400

    [CAP-41] add service to list of subjects

--- app/src/main/java/org/scesi/cappuchino/data/server/Service.kt
+++ app/src/main/java/org/scesi/cappuchino/data/server/Service.kt
@@ -2,9 +2,17 @@ package org.scesi.cappuchino.data.server
 
 
 import org.scesi.cappuchino.data.server.models.CareerResponse
+import org.scesi.cappuchino.data.server.models.SubjectResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface Service {
     @GET("FCyT/index.json")
     suspend fun getCareers(): List<CareerResponse>
+
+    @GET("FCyT/{careerPath}/{subjectCode}.json")
+    suspend fun getSubjects(
+        @Path("careerPath") careerPath: String,
+        @Path("subjectCode") subjectCode: String
+    ): SubjectResponse
 }

fc4e87adba9ffa96f3f2030c57ed0bafbcfbfefb
MauriNestor <mauric845apaza@gmail.com>
2025-06-02 23:00:34 -0400

    [CAP-41] modfied appdatabase and add dao for subejcts anidados

--- app/src/main/java/org/scesi/cappuchino/data/database/AppDataBase.kt
+++ app/src/main/java/org/scesi/cappuchino/data/database/AppDataBase.kt
@@ -2,8 +2,20 @@ package org.scesi.cappuchino.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import org.scesi.cappuchino.data.database.career.Career
+import org.scesi.cappuchino.data.database.career.CareerDao
+import org.scesi.cappuchino.data.database.subject.GroupEntity
+import org.scesi.cappuchino.data.database.subject.LevelEntity
+import org.scesi.cappuchino.data.database.subject.ScheduleEntity
+import org.scesi.cappuchino.data.database.subject.SubjectDao
+import org.scesi.cappuchino.data.database.subject.SubjectDetailEntity
+import org.scesi.cappuchino.data.database.subject.SubjectEntity
 
-@Database(entities = [Career::class], version = 2, exportSchema = false)
+@Database(
+    entities = [Career::class, SubjectEntity::class, LevelEntity::class, SubjectDetailEntity::class,
+        GroupEntity::class, ScheduleEntity::class], version = 14, exportSchema = false
+)
 abstract class AppDataBase: RoomDatabase() {
     abstract fun careerDao(): CareerDao
+    abstract fun subjectDao(): SubjectDao
 }
--- app/src/main/java/org/scesi/cappuchino/data/database/subject/SubjectDao.kt
+++ app/src/main/java/org/scesi/cappuchino/data/database/subject/SubjectDao.kt
@@ -1,4 +1,61 @@
 package org.scesi.cappuchino.data.database.subject
 
+
+import android.util.Log
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import org.scesi.domain.models.SearchCategory
+
+@Dao
 interface SubjectDao {
+    @Transaction
+    @Query("SELECT * FROM subjects WHERE code = :code AND path = :path")
+    suspend fun getSubjectWithLevels(code: String, path: String): SubjectWithLevels?
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubject(subject: SubjectEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertLevel(level: LevelEntity): Long
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubjectDetail(subject: SubjectDetailEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertGroup(group: GroupEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSchedule(schedule: ScheduleEntity)
+
+    @Transaction
+    suspend fun insertFullSubject(subject: SearchCategory.Subject) {
+        val subjectEntity = subject.toEntity()
+        insertSubject(subjectEntity)
+
+        subject.levels.forEach { level ->
+            val levelEntity = level.toEntity(subject.code)
+            insertLevel(levelEntity)
+
+            level.subjects.forEach { domainSubject ->
+                val subjectDetailEntity = domainSubject.toEntity(level.code)
+                insertSubjectDetail(subjectDetailEntity)
+
+                domainSubject.groups.forEach { group ->
+                    val groupEntity = group.toEntity(domainSubject.code)
+                    insertGroup(groupEntity)
+
+                    group.schedule.forEach { schedule ->
+                        val scheduleEntity = schedule.toEntity(group.code)
+                        insertSchedule(scheduleEntity)
+                    }
+                }
+            }
+        }
+    }
+
 }
+
